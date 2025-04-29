5ca3ff05cd53fb5111950824ff755e8ec0d95917
Daniel P.K <danilofletcher978@gmail.com>
2025-04-29 09:48:28 +0000

    first commit

---
+++ arrow.py
@@ -0,0 +1,20 @@
+#I am gonna find you Arrow Man 
+
+crime_scenes = {
+    "The Glades": 3,
+    "Starling City Bank": 5,
+    "Queen Consolidated": 4,
+    "Starling General Hospital": 6,
+    "Starling City Port": 8,
+    "CNRI law Office": 2      
+}
+
+#Now let's Map this out and by this I mean star city into a grid. 
+locations = {
+    "The Glades": (2, 8),
+    "Starling City Bank": (6, 5),
+    "Queen Consolidated": (5, 6),
+    "Starling General Hospital": (8, 7),
+    "Starling City Port": (1, 1),
+    "CNRI law Office": (3, 7)     
+}
